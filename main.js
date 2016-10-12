51ff71f207c15a0c60a77fd6cbd2d8884b061848
Amin Meyghani <meyghania@gmail.com>
2016-10-12 10:48:44 -0700

    add views + head partial.

--- main.js
+++ main.js
@@ -1,20 +1,57 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const DB_NAME = 'auth-demo';
 const MONGO_URL = `mongodb://localhost:27017/${DB_NAME}`;
 const mongoose = require('mongoose');
 mongoose.connect(MONGO_URL);
 
-const User = require('./models/user');
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-
-User.findOne({name: 'tom'}, function(err, user) {
-  if (err) {
-    throw err;
-  }
-  console.log(user.name);
+app.get('/', (req, res) => {
+    res.render('index.ejs');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login.ejs', {message: ''});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const User = require('./models/user');
+
+
+
+
+
+
+
+// User.findOne({name: 'Tom'}, function(err, user) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(user.name);
+// });
+
+
+app.listen(8189, a => console.log('running at port 8189'));
